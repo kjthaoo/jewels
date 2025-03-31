09521bc8b3b81b6e7ca041a5adafea7b50ba0f44
Kelly Thao <kjthao@email.sc.edu>
2025-03-31 04:29:29 -0400

    fixed navigation and toggle

--- my-jewel-app/src/components/Navigation.jsx
+++ my-jewel-app/src/components/Navigation.jsx
@@ -1,20 +1,28 @@
-import {Outlet, Link} from "react-router-dom";
+import { Link } from "react-router-dom";
 import './css/Navigation.css';
 
 function Navigation() {
-    return (
-        <nav id="main-nav">
-            <ul>
-                <li><Link to="/">Home</Link></li>
-                <li><Link to="/shop">Shop</Link></li>
-                <li><Link to="/about">About</Link></li>
-                <li><Link to="/customdesign">Custom Design</Link></li>
-                <li><Link to="/contactus">Contact Us</Link></li>
-                <li><Link to="/faqs">FAQS</Link></li>
-            </ul>
-        </nav>       
+  const toggleNav = () => {
+    const navItems = document.getElementById('nav-items');
+    navItems.classList.toggle('hide-small');
+  };
 
-    );
+  return (
+    <nav id="main-nav">
+      <div id="toggle-nav" onClick={toggleNav}>
+        <div></div>
+        <div></div>
+        <div></div>
+      </div>
+      <ul id="nav-items" className="nav-links hide-small">
+        <li><Link to="/shop">Shop</Link></li>
+        <li><Link to="/about">About Us</Link></li>
+        <li><Link to="/customdesign">Custom Design</Link></li>
+        <li><Link to="/contactus">Contact Us</Link></li>
+        <li><Link to="/faqs">FAQS</Link></li>
+      </ul>
+    </nav>
+  );
 };
 
 export default Navigation;
