b8cdaef2b36f068d16b27780c82695522bc65238
Kelly Thao <kjthao@email.sc.edu>
2025-04-14 07:32:00 -0400

    fixing header.css

--- my-jewel-app/src/components/HousePlans.jsx
+++ my-jewel-app/src/components/HousePlans.jsx
@@ -1,33 +1,62 @@
 import {useState, useEffect} from "react";
+import "./css/HousePlans.css";
 import axios from "axios";
 import HousePlan from "./HousePlan";
+import AddHousePlan from "./AddHousePlan";
 
 //https://portiaportia.github.io/json/house-plans.json
 const HousePlans = () => {
-    const [houses, setHousePlans] = useState([]); //defaults to an empty array
-    
-    // after page loaded to asynch json retrieval
+    const [houses, setHouses] = useState([]);
+    const [showAddDialog, setShowAddDialog] = useState(false);
+
+    //after page loaded to asynch json retrieval
     useEffect(()=>{
-        // automatically execute the asyn function (it doesnt have a name to it or need a name to if since we are only callign it once)
-        (async () => { // all the code thats fetching from the json file will be in here
-            const response = await axios.get("https://localhost:3001/api/houses"); //this client side is accessing the server (server.js)
-            setHousePlans(response.data);
-        })(); 
+        //automatically execute the async function
+        (async () => {
+            const response = await axios.get("http://localhost:3001/api/houses");
+            setHouses(response.data);
+        })();
+
     },[]);
 
-    return (
-        <div id="house-plans" className="columns">
-            {houses.map((house)=>( //for every house, we will create an component for it
-                 <HousePlan
-                 _id="1"
-                 name= {house.name} //using the name from the JSON data 
-                 size= {house.size} //all the keys in here must match the JSON data keys
-                 bedrooms={house.bedrooms}
-                 bathrooms={ house.bathrooms}
-                 main_image={house.main_image}/>
+    const openAddDialog = () => {
+        setShowAddDialog(true);
+    }
 
-            ))}
-        </div>
+    const closeAddDialog = () => {
+        console.log("I'm in the close method")
+        setShowAddDialog(false);
+    }
+
+    const updateHousePlans = (housePlan) => {
+        setHouses((houses)=>[...houses, housePlan]);
+    };
+
+
+    return (
+        <>
+            <button id="add-house" onClick={openAddDialog}>+</button>
+
+            {showAddDialog?(<AddHousePlan 
+                                closeAddDialog={closeAddDialog} 
+                                updateHousePlans={updateHousePlans}
+                                /> ): ("")}
+            
+            <div id="house-plans" className="columns">
+                {houses.map((house)=>(
+                    <HousePlan
+                    key={house.name}
+                    _id={houses._id}
+                    name={house.name}
+                    size={house.size}
+                    bedrooms={house.bedrooms}
+                    bathrooms={house.bathrooms}
+                    main_image={house.main_image}/>
+                ))}
+                
+            </div>
+        </>
+        
     );
 };
 
